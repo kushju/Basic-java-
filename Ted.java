0a45457955a57a9294ea146cabf3483072a7bf20
kuSh madaan <32204425+kushju@users.noreply.github.com>
2017-10-20 11:38:25 +0530

    Add files via upload

---
+++ Ted.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Ted{
+	public static void main(String[] args){
+		int a,b,c;
+		    Scanner in = new Scanner(System.in);
+		    System.out.println("enter the no.::");
+
+a=in.nextInt();
+b=in.nextInt();
+c=in.nextInt();
+
+if((b>a) && (c>b)){
+	System.out.println("increasing");
+
+}	
+if((b<a) &&(b<c)){
+	System.out.println("Decreasing");
+}	    
+
+
+		    }
+		 	
+		    }
+	
+
