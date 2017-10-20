07abb64833421847479325330e808fb4bd3ded77
kuSh madaan <32204425+kushju@users.noreply.github.com>
2017-10-20 22:32:43 +0530

    Add files via upload

---
+++ Ut.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Ut{
+	public static void main(String[] args){
+	int i,j,sp,n;
+		    Scanner in = new Scanner(System.in);
+		    System.out.println("enter the no. of rows::");
+n=in.nextInt();
+for(i=0;i<n;i++){
+
+	for(sp=n-i;sp>0;sp--){
+		System.out.print(" ");
+	}
+	for(j=0;j<i;j++){
+		System.out.print("z");
+	}
+	System.out.println();
+}
+
+
+
+
+		    }
+		 	
+		    }
+	
+
