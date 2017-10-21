8736509ea99924eb56d6009f4996669433c30b5e
kuSh madaan <32204425+kushju@users.noreply.github.com>
2017-10-21 16:22:52 +0530

    Add files via upload

---
+++ Ar1.java
@@ -0,0 +1,16 @@
+    import java.util.Scanner;
+    public class Ar1{
+	public static void main(String[] args){
+	int []arr = {1,2,3,4,5,6,7,8,9};
+	int sum=0;
+	int i;
+	for(i=0;i<arr.length;i++){
+		sum = sum + arr[i];
+	}
+System.out.print(sum);
+                   
+		    }
+		 	
+		    }
+	
+
