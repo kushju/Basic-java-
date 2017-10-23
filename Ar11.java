bfc162e26f34b53b749e3bd578be04ad3b7e28f8
kuSh madaan <32204425+kushju@users.noreply.github.com>
2017-10-24 01:14:47 +0530

    Add files via upload

---
+++ Ar11.java
@@ -0,0 +1,20 @@
+    import java.util.Arrays;
+    public class Ar11{
+	public static void main(String []args){
+		int []myarray1 = {1,22,22,33,4,98,98,97,44,3,7,77,88,7,2,67,78,1};
+         int myarray2[] = {1,2,34,5,6,8,9};
+	int i,j;
+	for(i=0;i<myarray1.length;i++){
+		for(j=0;j<myarray2.length;j++){
+			if(myarray1[i] == myarray2[j]){
+				System.out.println("common elemnts are : " + (myarray1[i]));
+			}
+		}
+	}	
+		
+	}
+	}
+		 	
+		    
+	
+
---
+++ Prime.java
@@ -0,0 +1,22 @@
+    import java.util.Scanner;
+    public class Prime{
+	public static void main(String []args){
+	Scanner in = new Scanner(System.in);
+	int num = in.nextInt();
+	int i;
+	for(i=2;i<num;i++){
+		if( num%i == 0){
+      System.out.println("np");
+		}
+		else{
+			System.out.println("prime");
+		}
+	}
+	}	
+		
+	}
+	
+		 	
+		    
+	
+
